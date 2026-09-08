7c53448355164c6fac6dc0c4f2f0c523ec9ee0c9
Sjoerd Brandsma <sjoerd@horus.nu>
2026-09-08 23:09:58 +0200

    Fix applet installer archive layout

--- build/package.ps1
+++ build/package.ps1
@@ -6,12 +6,12 @@ $dist = Join-Path $Root 'dist'
 New-Item -ItemType Directory -Force $dist | Out-Null
 $xml = @'
 <?xml version="1.0" encoding="UTF-8"?>
-<extensions><extension name="SpotifyConnect" version="0.3.0" target="baby"><file>SpotifyConnect/</file></extension></extensions>
+<extensions><extension name="SpotifyConnect" version="0.3.1" target="baby"><file>SpotifyConnect/</file></extension></extensions>
 '@
 Set-Content (Join-Path $dist 'extensions.xml') $xml -NoNewline
-$zip = Join-Path $dist 'SpotifyConnect-0.3.0.zip'
+$zip = Join-Path $dist 'SpotifyConnect-0.3.1.zip'
 if (Test-Path $zip) { Remove-Item $zip }
-Compress-Archive -Path (Join-Path $stage 'SpotifyConnect') -DestinationPath $zip
+Compress-Archive -Path (Join-Path $stage 'SpotifyConnect/*') -DestinationPath $zip
 $sha1 = (Get-FileHash (Join-Path $dist 'extensions.xml') -Algorithm SHA1).Hash
 Set-Content (Join-Path $dist 'extensions.xml.sha1') $sha1
 Write-Host "Package: $zip"
